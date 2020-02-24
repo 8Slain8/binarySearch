33cbe7083535c5edf0b06d72b9618c64dff58c27
8Slain8 <arion_sinagra@outlook.com>
2020-02-24 19:37:54 +0100

    Initial Commit

---
+++ 24.02.2020_classBinarySearch/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  24.02.2020_classBinarySearch
+//
+//  Created by Productivity on 24.02.20.
+//  Copyright © 2020 Arion. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
